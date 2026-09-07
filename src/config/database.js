32bf724a288712ac1d54361baa3d5734a0c84135
Lucas Rojas <Lucasroket203@gmail.com>
2026-09-07 19:12:25 -0300

    Creo database.js (closes #9)

---
+++ src/config/database.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+// Pruebo la conexión al iniciar
+pool.getConnection()
+    .then(connection => {
+        console.log(' Conexión exitosa a MySQL: ' + process.env.DB_NAME);
+        connection.release();
+    })
+    .catch(err => {
+        console.error(' Error conectando a la Base de Datos:', err.message);
+    });
+
+module.exports = pool;
